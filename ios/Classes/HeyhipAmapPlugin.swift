c5e8a2677a8e51db3c51b33fc4a060840363c2d9
蛤蟆老弟的好朋友 <heyhip@outlook.com>
2026-01-18 06:19:43 -0500

    修改

--- ios/Classes/HeyhipAmapPlugin.swift
+++ ios/Classes/HeyhipAmapPlugin.swift
@@ -16,21 +16,8 @@ public class HeyhipAmapPlugin: NSObject, FlutterPlugin {
 
   }
 
-  // public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-  //   switch call.method {
-  //   case "getPlatformVersion":
-  //     result("iOS " + UIDevice.current.systemVersion)
-  //   default:
-  //     result(FlutterMethodNotImplemented)
-  //   }
-  // }
-
-
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     switch call.method {
-    case "initAmap":
-      handleInitAmap(call, result)
-      break
     case "getPlatformVersion":
       result("iOS " + UIDevice.current.systemVersion)
     default:
@@ -39,47 +26,9 @@ public class HeyhipAmapPlugin: NSObject, FlutterPlugin {
   }
 
 
-  private func handleInitAmap(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    guard let args = call.arguments as? [String: Any] else {
-      result(
-        FlutterError(
-          code: "INVALID_ARGS",
-          message: "Arguments are not a dictionary",
-          details: nil
-        )
-      )
-      return
-    }
 
-    let apiKey = args["apiKey"] as? String
-    let agreePrivacy = args["agreePrivacy"] as? Bool
 
-    if apiKey == nil || apiKey!.isEmpty {
-      result(
-        FlutterError(
-          code: "INVALID_API_KEY",
-          message: "apiKey is empty",
-          details: nil
-        )
-      )
-      return
-    }
 
-    if agreePrivacy != true {
-      result(
-        FlutterError(
-          code: "PRIVACY_NOT_AGREED",
-          message: "User has not agreed privacy policy",
-          details: nil
-        )
-      )
-      return
-    }
-
-    // AMapServices.shared().apiKey = apiKey!
-
-    result(nil)
-  }
 
 
 }
